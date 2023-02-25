66c1e3977194f6453e8476d315c64a6a884fe039
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-02-25 13:24:06 -0500

    Include getJsonObject as global function

--- global.js
+++ global.js
@@ -128,3 +128,27 @@ const icon_by_name = {
     //## Missing stuff
     //"Garbage Worm": "00_clear",
 }
+
+function getJsonObject(url, cb, async = true) {
+    if (lastRequest != null) lastRequest.abort();
+    let request = new XMLHttpRequest();
+    lastRequest = request;
+    requestLock = {};
+    request.requestLock = requestLock;
+    request.open('GET', url, async);
+    request.onreadystatechange = function () {
+        if (request.requestLock != requestLock && request.status != 0) {
+            request.abort();
+            console.log("request for " + url + " aborted!");
+        }
+        else if (request.readyState === 4 && request.status === 200) {
+            try {
+                cb(JSON.parse(request.responseText));
+            } catch (err) {
+                console.log(err);
+            }
+            if (lastRequest == request) lastRequest = null;
+        }
+    }
+    request.send();
+}
